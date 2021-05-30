f6366def85c24b5e723b60685cd5a1e4ea159ec3
OM Miles <miles@ommiles.com>
2021-05-29 20:38:17 -0700

    tachyons added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className=''>
+      <header className=''>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <h1>
+          Hello World
+        </h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className=''
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
