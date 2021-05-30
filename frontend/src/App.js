463643b5d9bd8143367ffc3db8c671edcd94b32d
OM Miles <miles@ommiles.com>
2021-05-29 22:15:39 -0700

    add erd gem and change App.js to class component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 import './css/App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className=''>
-      <header className=''>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
-        <h1>
-          Hello World
-        </h1>
-        <a
-          className=''
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    fetch('localhost:9393/test')
+      .then(res => res.json)
+      .then(console.log)
+  }
+
+  render() {
+    return (
+      <div className=''>
+        <header className=''>
+          {/* <img src={logo} className='App-logo' alt='logo' /> */}
+          <h1>
+            Hello World
+          </h1>
+          <a
+            className=''
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
