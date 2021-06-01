25e68c60f679abc5d4e1328e4e93d6c73dae37c1
OM Miles <miles@ommiles.com>
2021-05-31 22:41:05 -0700

    user class ar set up

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+    has_many :activities
+    has_many :appointments
+    has_many :caregivers
+    has_many :pharmacies
+    has_many :prescriptions, through: :pharmacies
+    has_many :physicians
 end
